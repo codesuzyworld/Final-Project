e90769083c0b2d209be166bc421ff0a99d4b7bcb
suzanna8767 <codessuzy@gmail.com>
2024-08-13 03:24:46 -0400

    Update

--- script/game.js
+++ script/game.js
@@ -2,27 +2,31 @@
 //Listen for window load the jQuery way
 $(document).ready(function () {
 
-    //Start Game
-    let startGameSection = document.getElementById("startGame");
-    let startGameBtn = document.getElementById("startGameBtn");
+    // //Start Game
+    // let startGameSection = document.getElementById("startGame");
+    // let startGameBtn = document.getElementById("startGameBtn");
 
-    // Timer and Score 
-    let timer = document.getElementById("timer");
-    let score = document.getElementById("score");
+    // // Timer and Score 
+    // let timer = document.getElementById("timer");
+    // let score = document.getElementById("score");
 
-    //Whack A Mole
-    let whack1 = document.getElementById("whack1");
-    let whack2 = document.getElementById("whack2");
-    let whack3 = document.getElementById("whack3");
-    let whack4 = document.getElementById("whack4");
-    let whack5 = document.getElementById("whack5");
-    let whack6 = document.getElementById("whack6");
+    // //Whack A Mole
+    // let whack1 = document.getElementById("whack1");
+    // let whack2 = document.getElementById("whack2");
+    // let whack3 = document.getElementById("whack3");
+    // let whack4 = document.getElementById("whack4");
+    // let whack5 = document.getElementById("whack5");
+    // let whack6 = document.getElementById("whack6");
+
+    // Play menu animation, everyone jumping in and out! 
+    $(".mouse").addClass("mouseMenuAnimation");
 
     // Function that will choose a random hole 
     function chooseHole(){
         let randomHole = Math.floor(Math.random() * 6);
         console.log(randomHole);
         $(".mouse").removeClass("mouseAnimation");
+        
         if (randomHole === 0){
             $("#mouse1").addClass("mouseAnimation");
         } else if (randomHole === 1){
@@ -38,8 +42,15 @@ $(document).ready(function () {
         }
     }
 
-    $(startGameBtn).on("click", function() {
+    //Function that will start the game after clickint the start game button. 
+    function startGame(){
+        $("#startGameBtn").hide();
+        $(".mouse").removeClass("mouseMenuAnimation");
         chooseHole();
+    }
+
+    $("#startGameBtn").on("click", function() {
+        startGame();
     });
 
 });
