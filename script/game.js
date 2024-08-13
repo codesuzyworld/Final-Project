38a4532534c74a9927ec96ee83bc615f539e7929
suzanna8767 <codessuzy@gmail.com>
2024-08-13 04:24:09 -0400

    Finished hit box and score functionality

--- script/game.js
+++ script/game.js
@@ -6,17 +6,9 @@ $(document).ready(function () {
     // let startGameSection = document.getElementById("startGame");
     // let startGameBtn = document.getElementById("startGameBtn");
 
-    // // Timer and Score 
-    // let timer = document.getElementById("timer");
-    // let score = document.getElementById("score");
-
-    // //Whack A Mole
-    // let whack1 = document.getElementById("whack1");
-    // let whack2 = document.getElementById("whack2");
-    // let whack3 = document.getElementById("whack3");
-    // let whack4 = document.getElementById("whack4");
-    // let whack5 = document.getElementById("whack5");
-    // let whack6 = document.getElementById("whack6");
+    // Timer and Score 
+    let timer = document.getElementById("timer");
+    let score = document.getElementById("score");
 
     // Play menu animation, everyone jumping in and out! 
     $(".mouse").addClass("mouseMenuAnimation");
@@ -42,13 +34,70 @@ $(document).ready(function () {
         }
     }
 
+
+    //Timer Function for 30 seconds
+
+    //30 Seconds of game time = 10000 miliseconds
+    let gameTime = 30000;
+    //Set remaining time
+    let remainingTime = gameTime;
+    //Interval is by default null
+    let interval = null;
+    //Score is by default 0
+    let gameScore = 0;
+
+    function gameTimer(){
+
+        // If time is up, then clear the interval and score, and stop the game
+        if (remainingTime <= 0){
+            window.clearInterval(interval);
+            timer.innerHTML = "0 Sec";
+            gameScore = 0;
+            score.innerHTML = gameScore + " Points";
+            $("#startGameBtn").show();
+            return;
+        }
+
+        // Everytime this function runs, deduct 100 miliseconds off the remaining time.
+        remainingTime -= 1000;
+        chooseHole();
+        //Create time variables 
+        // Turn the miliseconds into seconds
+        let nowSeconds = Math.floor(remainingTime / 1000);
+        timer.innerHTML = nowSeconds + "Sec";
+
+    }
+
     //Function that will start the game after clickint the start game button. 
     function startGame(){
         $("#startGameBtn").hide();
         $(".mouse").removeClass("mouseMenuAnimation");
-        chooseHole();
+        gameTimer();
+        timer.innerHTML = Math.floor(gameTime / 1000) + "Sec";
+		// The interval will be 1 second, and will run the timer function 
+        interval = window.setInterval(gameTimer, 1000);
+
+        //If the player clicks the mouse container, then 10 points will be added
+        $(".whack").on("click", function() {
+            //If the animation is playing, then it counts as a hit
+            if ($(this).find(".mouse").hasClass("mouseAnimation")) {
+                console.log("10 points added");
+                gameScore += 10;
+                score.innerHTML = gameScore + " Points";
+
+                // Set the mouse element for easy access
+                let mouseElement = $(this).find(".mouse");
+                //Play hit animation 
+                mouseElement.addClass("mouseHitAnimation");
+                // Remove the hit animation class after it ends
+                mouseElement.on('animationend', function() {
+                    mouseElement.removeClass("mouseHitAnimation");
+                });
+            }
+        });
     }
 
+    //Event Listener
     $("#startGameBtn").on("click", function() {
         startGame();
     });
